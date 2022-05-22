163b95577561fd7262c40e9944c7f8905b1945c3
Rakib-Dev <rhbappy456@gmail.com>
2022-05-23 00:57:15 +0600

    create nested routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import { publicRoute } from './Routes/publicRoutes';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
+import { privateRoutes } from './Routes/privateRoutes';
+import PrivateRoute from './authentication/PrivateRoute';
+import AdminRoute from './authentication/AdminRoute';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import AddAdmin from './Pages/Dashboard/AddAdmin';
+import AddService from './Pages/Dashboard/AddService';
 
 
 
@@ -13,11 +19,25 @@ function App() {
   }, [])
   return (
     <>
-    <Navbar>
-      <Routes>
-        {publicRoute.map(({path, Component}, index) => <Route path={path} key={index} element={<Component/>} />)}
-      </Routes>
-    </Navbar>
+      <Navbar>
+        <Routes>
+          {publicRoute.map(({ path, Component }, index) => <Route path={path} key={index} element={<Component />}
+          />)}
+
+          <Route element={<PrivateRoute />}>
+            {privateRoutes.map(({ path, Component }, index) => (
+              <Route key={index} path={path} element={<Component />} />
+            ))}
+          </Route>
+          <Route element={<AdminRoute />}>
+            <Route path='/dashboard' element={<Dashboard />}>
+              <Route path='add-admin' element={<AddAdmin />} />
+              <Route path='add-service' element={<AddService />} />
+            </Route>
+          </Route>
+
+        </Routes>
+      </Navbar>
     </>
   );
 }
