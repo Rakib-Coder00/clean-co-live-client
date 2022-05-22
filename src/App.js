b76ed8da7bafd01a078834997ce6c31919b7ba19
Rakib-Dev <rhbappy456@gmail.com>
2022-05-22 22:54:18 +0600

    implement AOS animation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import { publicRoute } from './Routes/publicRoutes';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+
 
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+  }, [])
   return (
     <>
     <Navbar>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import About from './About';
+import Landing from './Landing';
 
 const Home = () => {
     return (
         <div>
+            <Landing />
             <About/>
         </div>
     );
